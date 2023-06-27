28e86554c6ee9cee4e263a7fae8de280fb6f1fb9
tanmayhaldar27 <130904109+tanmayhaldar27@users.noreply.github.com>
2023-06-28 00:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function convertToBinary() {
+  // Get the decimal number input value
+  var decimalInput = document.getElementById("decimal-input").value;
+
+  // Convert decimal to binary
+  var binaryOutput = decimalToBinary(decimalInput);
+
+  // Set the binary number output value
+  document.getElementById("binary-output").value = binaryOutput;
 }
 
-module.exports = threeSum;
+function decimalToBinary(decimal) {
+  // Check for the base case
+  if (decimal === 0) {
+    return "0";
+  }
+
+  var binary = "";
+
+  // Convert decimal to binary using division and remainder
+  while (decimal > 0) {
+    var remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
+}
