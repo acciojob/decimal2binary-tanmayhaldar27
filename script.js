f06f79146bd2262676e25a97c5bd0c6e1e131028
tanmayhaldar27 <130904109+tanmayhaldar27@users.noreply.github.com>
2023-07-03 21:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-function convertToBinary() {
-  // Get the decimal number input value
-  var decimalInput = document.getElementById("decimal-input").value;
-
-  // Convert decimal to binary
-  var binaryOutput = decimalToBinary(decimalInput);
-
-  // Set the binary number output value
-  document.getElementById("binary-output").value = binaryOutput;
-}
-
 function decimalToBinary(decimal) {
-  // Check for the base case
-  if (decimal === 0) {
-    return "0";
-  }
-
-  var binary = "";
-
-  // Convert decimal to binary using division and remainder
-  while (decimal > 0) {
-    var remainder = decimal % 2;
-    binary = remainder + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+  return decimal.toString(2);
 }
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
